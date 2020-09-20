00d528444ad155ed2d8c77154fb321dd9ea13e11
ruagsm9 <Maksim.GUSTOV@raiffeisen.ru>
2020-09-20 13:16:51 +0300

    Lab1

---
+++ testservice/src/main/java/edu/bigdata/training/testservice/service/TestBusinessLogicService.java
@@ -0,0 +1,41 @@
+package edu.bigdata.training.testservice.service;
+
+import edu.bigdata.training.testservice.controller.model.Person;
+import edu.bigdata.training.testservice.dao.TestServiceRepository;
+import edu.bigdata.training.testservice.model.PersonEntity;
+
+import java.util.List;
+import java.util.UUID;
+
+public class TestBusinessLogicService {
+
+    private TestServiceRepository testServiceRepository;
+
+    public TestBusinessLogicService(TestServiceRepository testServiceRepository) {
+        this.testServiceRepository = testServiceRepository;
+    }
+
+    public PersonEntity processCreate(Person person){
+        PersonEntity personEntity = new PersonEntity(person.getName());
+        testServiceRepository.save(personEntity);
+        return personEntity;
+    }
+
+    public void processDel(String id){
+        testServiceRepository.del(UUID.fromString(id));
+    }
+
+    public PersonEntity processUpdate(String id, Person person){
+        PersonEntity personEntity = new PersonEntity(person.getName());
+        personEntity.setId(UUID.fromString(id));
+        return testServiceRepository.update(personEntity.getId(), personEntity);
+    }
+
+    public PersonEntity processGet(String id){
+        return testServiceRepository.get(UUID.fromString(id));
+    }
+
+    public List<PersonEntity> processGetAll(){
+        return testServiceRepository.getAll();
+    }
+}
