8ee9bfccf8c6cbc6e25f8721b36c3a3b7ffbb7a2
ruagsm9 <Maksim.GUSTOV@raiffeisen.ru>
2021-01-13 18:05:22 +0300

    lab7

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ docker push "$1"
 
 kubectl apply -f test-service-configmap.yaml
 cd src/main/resources/keystore/local/
-kubectl create secret generic testservicesecret --from-literal=keystore_password=1234567890 --from-file=keystore.p12=keystore.p12 --from-literal=truststore_password=1234567890 --from-file=truststore.jks=truststore.jks
+#kubectl create secret generic testservicesecret --from-literal=keystore_password=1234567890 --from-file=keystore.p12=keystore.p12 --from-literal=truststore_password=1234567890 --from-file=truststore.jks=truststore.jks
 
 cd ../../../../..
 sed "s%javapipe2020/testservice:ssl%$1%g" test-service-deployment.yaml > deployment.yaml
